c44cf274e3b4307471edbd7f837642951b199722
Robert Newson <rnewson@apache.org>
2018-07-31 15:39:40 +0100

    Support partitioned flag at db creation time

--- src/chttpd/src/chttpd_db.erl
+++ src/chttpd/src/chttpd_db.erl
@@ -285,11 +285,12 @@ create_db_req(#httpd{}=Req, DbName) ->
     Q = chttpd:qs_value(Req, "q", config:get("cluster", "q", "8")),
     P = chttpd:qs_value(Req, "placement", config:get("cluster", "placement")),
     EngineOpt = parse_engine_opt(Req),
+    PartitionedOpt = parse_partitioned_opt(Req),
     Options = [
         {n, N},
         {q, Q},
         {placement, P}
-    ] ++ EngineOpt,
+    ] ++ EngineOpt ++ PartitionedOpt,
     DocUrl = absolute_uri(Req, "/" ++ couch_util:url_encode(DbName)),
     case fabric:create_db(DbName, Options) of
     ok ->
@@ -1411,6 +1412,18 @@ parse_engine_opt(Req) ->
                     throw({bad_request, invalid_engine_extension})
             end
     end.
+
+
+parse_partitioned_opt(Req) ->
+    case chttpd:qs_value(Req, "partitioned") of
+        undefined ->
+            [];
+        "true" ->
+            [partitioned];
+        _ ->
+            throw({bad_request, <<"`partitioned` parameter can only be set to true.">>})
+    end.
+
 
 parse_doc_query({Key, Value}, Args) ->
     case {Key, Value} of
